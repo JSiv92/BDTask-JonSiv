7f0cdb4e82d2b9b28f245dbf6704e8388fa9d19c
Jonathan <78221006+JSiv92@users.noreply.github.com>
2023-08-29 11:48:51 +1200

    created controller and views for Customers. -Customer/Index() for viewing all customer -Customer/Create() to add a customer -added navbar links

---
+++ BDTask-JonSiv/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BDTask_JonSiv.Models.DB;
+
+namespace BDTask_JonSiv.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //empty list of Customer type to store newly created customers for purpose of the test
+        private static List<Customer> _customers = new List<Customer>();
+
+        public IActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        //GET method to get the Customer create form:
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST method to add the new customer to the _customer list on submit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _customers.Add(customer);
+            }
+            return View(customer);
+        }
+    }
+}
