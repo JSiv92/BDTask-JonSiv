e5b891dc5ec7ba7cbe259f1ec316bd17b19c729a
Jonathan <78221006+JSiv92@users.noreply.github.com>
2023-08-29 12:39:11 +1200

    created viewmodel to show customer and their order count

---
+++ BDTask-JonSiv/ViewModels/CustomerOrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BDTask_JonSiv.Models.DB;
+
+namespace BDTask_JonSiv.ViewModels
+{
+    //custom viewmodel in order to join customer and orders table to get order count per customer id
+    public class CustomerOrdersViewModel
+    {
+        public Customer? Customer { get; set; }
+        public int OrderCount { get; set; }
+    }
+}
